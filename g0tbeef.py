98d0741ed6ffc65145e331ab985711de911927af
levle <matt.bergin@hotmail.com>
2013-05-14 18:22:18 -0600

    Update g0tbeef.py

--- g0tbeef.py
+++ g0tbeef.py
@@ -6,24 +6,16 @@
 #       Email: level@coresecurity.com
 #
 #
-# Copyright (c) CORE Security, CORE SDI Inc.
-# All rights reserved.
-#
-# This computer software is owned by Core SDI Inc. and is
-# protected by U.S. copyright laws and other laws and by international
-# treaties.  This computer software is furnished by CORE SDI Inc.
-# pursuant to a written license agreement and may be used, copied,
-# transmitted, and stored only in accordance with the terms of such
-# license and with the inclusion of the above copyright notice.  This
-# computer software or any other copies thereof may not be provided or
-# otherwise made available to any other person.
-#
-#
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL CORE SDI Inc. BE LIABLE
-# FOR ANY DIRECT,  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR
-# CONSEQUENTIAL  DAMAGES RESULTING FROM THE USE OR MISUSE OF
-# THIS SOFTWARE
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+#IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+#INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+#NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+#PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+#WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+#ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+#POSSIBILITY OF SUCH DAMAGE.
 #
 #--
 
